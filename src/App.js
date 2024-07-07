f79e4bc9a7a17bad4e080b2aca3745b3ef99a9c5
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-07 07:04:44 +0530

    Updated App.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import './global.css';
+import './font.css';
+import Header from './components/Header/Header';
+import TypeBox from './components/TypeBox/TypeBox';
 
 const App = () => {
   return (
     <div className='App'>
-
+      <Header />
+      <TypeBox />
     </div>
   )
 }
