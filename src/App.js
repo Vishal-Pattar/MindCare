b620e8ce2b21c616be929fed8f2b44cf04557983
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-10 23:32:11 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './global.css';
 import './font.css';
 import Header from './components/Header/Header';
 import TypeBox from './components/TypeBox/TypeBox';
+// import WelcomeBox from './components/WelcomeBox/WelcomeBox';
+import OutputBox from './components/OutputBox/OutputBox';
 
 const App = () => {
-  return (
-    <div className='App'>
-      <Header />
-      <TypeBox />
-    </div>
-  )
+    const [messages, setMessages] = useState([]);
+
+    const addMessage = (userMessage) => {
+        const newMessage = {
+            user: userMessage,
+            output: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea, eligendi repellat cupiditate eum blanditiis omnis nemo. Ut, quos nihil.'
+        };
+        setMessages([...messages, newMessage]);
+    };
+
+    return (
+        <div className='App'>
+            <Header />
+            <TypeBox addMessage={addMessage} />
+            <OutputBox messages={messages} />
+        </div>
+    )
 }
 
-export default App
+export default App;
