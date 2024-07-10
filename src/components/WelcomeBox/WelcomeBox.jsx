94b013797447cefc9d3cc6a9ef8dc5feeb90234c
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-10 23:01:27 +0530

    Added WelcomeBox

---
+++ src/components/WelcomeBox/WelcomeBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './WelcomeBox.css';
+
+const WelcomeBox = () => {
+  return (
+    <div className='welcomebox__container'>
+        <div className='welcomebox__header'>Welcome to React.js AI Chatbot!</div>
+        <div className='welcomebox__content'>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente tempore, optio praesentium eaque ducimus dolorum.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ut nobis quisquam impedit debitis vero deleniti! Dicta, aut corporis recusandae ipsam inventore cum incidunt aspernatur?</p>
+        </div>
+    </div>
+  )
+}
+
+export default WelcomeBox
