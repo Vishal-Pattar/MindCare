c50ae8fc239bc0c6f608cbb5e7f38020ea64bbe0
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-13 20:03:09 +0530

    Updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className='header__container'>
-        <span className='header__title roboto-regular'>ChatGPT</span>
-        <button className='header__button roboto-regular'>Login</button>
+        <div className='header__title roboto-regular'>ChatGPT</div>
+        <Link to="/login">
+            <button className='header__button roboto-regular'>Login</button>
+        </Link>
     </div>
-  )
+  );
 }
 
 export default Header;
