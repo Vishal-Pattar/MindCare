89d70ce5352b5843818fb90ac90eee89437a75de
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-13 20:05:00 +0530

    Added RegisterBox

---
+++ src/components/RegisterBox/RegisterBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './RegisterBox.css';
+import { Link } from 'react-router-dom';
+
+const RegisterBox = () => {
+    return (
+        <>
+        <div className='registerbox__container'>
+            <div className='registerbox__title'>Sign up for an account!</div>
+            <div className='registerbox__email registerbox__input'>
+                <label for="email">Email</label>
+                <input type='email' id='email' placeholder='Enter your email password' />
+            </div>
+            <div className='registerbox__password registerbox__input'>
+                <label for="pass">Password</label>
+                <input type='password' id='pass' placeholder='Enter Password' />
+            </div>
+            <button className='registerbox__button'>Create Account</button>
+        </div>
+        <div className='registerbox__footer'>
+            Already have an account? <Link to='/login'>Log in</Link></div>  
+        </>
+    )
+}
+
+export default RegisterBox;
