d92bdf3f96128a125cebcd930d058edcbc2be769
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-14 20:17:27 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import './global.css';
 import './font.css';
 import Header from './components/Header/Header';
 import TypeBox from './components/TypeBox/TypeBox';
 import OutputBox from './components/OutputBox/OutputBox';
-import LoginBox from './components/LoginBox/LoginBox';
-import RegisterBox from './components/RegisterBox/RegisterBox';
+import LoginBox from './components/SignBox/LoginBox';
+import RegisterBox from './components/SignBox/RegisterBox';
+import useMessage from './hooks/useMessage';
 
 const Main = ({ addMessage, messages }) => (
     <>
@@ -17,15 +18,7 @@ const Main = ({ addMessage, messages }) => (
 );
 
 const App = () => {
-    const [messages, setMessages] = useState([]);
-
-    const addMessage = (userMessage) => {
-        const newMessage = {
-            user: userMessage,
-            output: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea, eligendi repellat cupiditate eum blanditiis omnis nemo. Ut, quos nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi ea, eligendi repellat.'
-        };
-        setMessages([...messages, newMessage]);
-    };
+    const [messages, addMessage] = useMessage();
 
     return (
         <Router>
