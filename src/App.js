45e8b67ba584166c5b44e9170f03340786ea3d6d
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-17 23:51:13 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,11 @@ import './App.css';
 import './global.css';
 import './font.css';
 import Header from './components/Header/Header';
-import TypeBox from './components/TypeBox/TypeBox';
-import OutputBox from './components/OutputBox/OutputBox';
 import LoginBox from './components/SignBox/LoginBox';
 import RegisterBox from './components/SignBox/RegisterBox';
-import useMessage from './hooks/useMessage';
-
-const Main = ({ addMessage, messages }) => (
-    <>
-        <TypeBox addMessage={addMessage} />
-        <OutputBox messages={messages} />
-    </>
-);
+import HomeArea from './components/HomeArea/HomeArea';
 
 const App = () => {
-    const [messages, addMessage] = useMessage();
-
     return (
         <Router>
             <div className='App'>
@@ -27,7 +16,7 @@ const App = () => {
                 <Routes>
                     <Route path="/login" element={<LoginBox />} />
                     <Route path="/register" element={<RegisterBox />} />
-                    <Route path="/" element={<Main addMessage={addMessage} messages={messages} />} />
+                    <Route path="/" element={<HomeArea />} />
                 </Routes>
             </div>
         </Router>
