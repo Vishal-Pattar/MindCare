3437869c631d3e7c32590b744644ec0c3afb8fbb
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-24 03:41:47 +0530

    Updated register.js

--- netlify/functions/register.js
+++ netlify/functions/register.js
@@ -1,6 +1,11 @@
 const { MongoClient } = require('mongodb');
 const bcrypt = require('bcrypt');
 
+const getISTDate = () => {
+  const date = new Date();
+  return date.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+};
+
 exports.handler = async (req, res) => {
   if (req.httpMethod !== 'POST') {
     return {
@@ -20,7 +25,8 @@ exports.handler = async (req, res) => {
   try {
     const userByUsername = await users.findOne({ username });
     const userByEmail = await users.findOne({ email });
-    const referrer = await referrals.findOne({ referralCode: referral });
+    const referrer = await referrals.findOne({ referralCode: referral});
+    const referral_valid = await referrals.findOne({ referralCode: referral, usedBy: null });
 
     if (userByUsername) {
       return {
@@ -43,8 +49,16 @@ exports.handler = async (req, res) => {
       };
     }
 
+    if (!referral_valid) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ msg: 'Referral code has already been used. Please contact the administrator to obtain a new valid code' }),
+      };
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
     await users.insertOne({ username, password: hashedPassword, email, referral });
+    await referrals.updateOne({ referralCode: referral }, { $set: { usedBy: username, usedDate: getISTDate() } });
 
     await client.close();
     return {
