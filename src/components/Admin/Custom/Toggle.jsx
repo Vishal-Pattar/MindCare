daad32955672fce6814b7c64ee2b15f99a8b4aec
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-28 23:28:31 +0530

    Updated Toggle.jsx

--- src/components/Admin/Custom/Toggle.jsx
+++ src/components/Admin/Custom/Toggle.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './Toggle.css'
+import React from 'react';
+import './Toggle.css';
 
 const Toggle = ({ permit, onClick }) => {
     return (
@@ -7,7 +7,7 @@ const Toggle = ({ permit, onClick }) => {
             <input type="checkbox" checked={permit} onChange={onClick} />
             <span className="slider round"></span>
         </label>
-    )
-}
+    );
+};
 
-export default Toggle
+export default Toggle;
