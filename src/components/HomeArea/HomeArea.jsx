91e2b76a3424b7562c3fbdb0d6ceaa248c405821
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-31 09:29:28 +0530

    Updated HomeArea.jsx

--- src/components/HomeArea/HomeArea.jsx
+++ src/components/HomeArea/HomeArea.jsx
@@ -1,44 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import OutputBox from '../OutputBox/OutputBox';
 import TypeBox from '../TypeBox/TypeBox';
-import axios from 'axios';
 import useMessages from '../../hooks/useMessages';
 
 const HomeArea = () => {
-    const [messages, addMessage, clearMessages] = useMessages();
-    const [username, setUsername] = useState('');
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const token = localStorage.getItem('authToken');
-        if (!token) {
-            navigate('/admin');
-            return;
-        }
-
-        const fetchUser = async () => {
-            try {
-                const url = '/.netlify/functions/authentication';
-                const response = await axios.get(url, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    },
-                    withCredentials: true
-                });
-                if (response.data.username) {
-                    setUsername(response.data.username);
-                } else {
-                    navigate('/login');
-                }
-            } catch (error) {
-                console.error(error);
-                navigate('/login');
-            }
-        };
-
-        fetchUser();
-    }, [navigate]);
+    const [messages, addMessage] = useMessages();
 
     return (
         <>
