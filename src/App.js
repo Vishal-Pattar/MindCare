aa967f75b1f04cdbf7365cda41298d7b3545d40e
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-31 09:26:44 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Header from './components/Header/Header';
 import LoginBox from './components/SignBox/LoginBox';
 import RegisterBox from './components/SignBox/RegisterBox';
 import HomeArea from './components/HomeArea/HomeArea';
-import Admin from './components/Admin/Admin';
+import Admin from './components/Admin/Admin'
+import Users from './components/Admin/Users/Users';
+import Coupon from './components/Admin/Coupon/Coupon';
+import Session from './components/Admin/Session/Session';
+import ChatHistory from './components/Admin/ChatHistory/ChatHistory';
 
 const App = () => {
     return (
@@ -15,7 +19,12 @@ const App = () => {
             <div className='App'>
                 <Header />
                 <Routes>
-                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/admin" element={<Admin />}>
+                        <Route path="users" element={<Users />} />
+                        <Route path="coupons" element={<Coupon />} />
+                        <Route path="sessions" element={<Session />} />
+                        <Route path="history/:sessionId" element={<ChatHistory />} />
+                    </Route>
                     <Route path="/login" element={<LoginBox />} />
                     <Route path="/register" element={<RegisterBox />} />
                     <Route path="/" element={<HomeArea />} />
