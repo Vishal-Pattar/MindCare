7b393721d6c1aecbe953084dfa90d6714db81085
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-04 22:55:18 +0530

    Updated useMessages.js

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -1,41 +1,67 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
+import { useAlert } from "../context/AlertContext";
+import useCredits, { triggerFetchCredits } from "./useCredits";
 
 const useMessages = () => {
-    const [messages, setMessages] = useState([]);
-    const token = localStorage.getItem('authToken');
+  const [messages, setMessages] = useState([]);
+  const token = sessionStorage.getItem("authToken");
+  const { addAlert } = useAlert();
+  const credits = useCredits();
 
-    const addMessage = async (userMessage) => {
-        const tempMessageId = Date.now();
-        setMessages(prevMessages => [...prevMessages, { id: tempMessageId, user: userMessage, output: 'Loading...', loading: true }]);
+  const addMessage = async (userMessage) => {
+    if(credits <= 0){
+      addAlert("You have no credits left. Please purchase more to continue using the service.", "error", "signbox");
+      return;
+    }
+    const tempMessageId = Date.now();
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        id: tempMessageId,
+        user: userMessage,
+        output: "Loading...",
+        loading: true,
+      },
+    ]);
 
-        try {
-            const url = 'http://localhost:5000/api/generate';
-            const response = await axios.post(url, { prompt: userMessage }, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            setMessages(prevMessages => 
-                prevMessages.map(msg => 
-                    msg.id === tempMessageId ? { ...msg, output: response.data.text, loading: false } : msg
-                )
-            );
-        } catch (error) {
-            console.error('Error generating content:', error);
-            setMessages(prevMessages => 
-                prevMessages.map(msg => 
-                    msg.id === tempMessageId ? { ...msg, output: 'Error generating content. Please try again.', loading: false } : msg
-                )
-            );
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/v1/generate`,
+        { prompt: userMessage },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
-    };
+      );
 
-    const clearMessages = () => {
-        setMessages([]);
-    };
+      setMessages((prevMessages) =>
+        prevMessages.map((msg) =>
+          msg.id === tempMessageId
+            ? { ...msg, output: response.data.response, loading: false }
+            : msg
+        )
+      );
+      triggerFetchCredits();
+    } catch (error) {
+      console.error(error);
+      setMessages((prevMessages) =>
+        prevMessages.filter((msg) => msg.id !== tempMessageId)
+      );
+      addAlert(
+        error.response?.data?.error || "An error occurred",
+        "error",
+        "signbox"
+      );
+    }
+  };
 
-    return [messages, addMessage, clearMessages];
+  const clearMessages = () => {
+    setMessages([]);
+  };
+
+  return [messages, addMessage, clearMessages];
 };
 
 export default useMessages;
