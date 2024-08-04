8c9b4f4e2c224b3b31ca67e83fd893e12ce9444e
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-04 23:04:19 +0530

    Updated SignBox

--- src/components/SignBox/LoginBox.jsx
+++ src/components/SignBox/LoginBox.jsx
@@ -1,79 +1,96 @@
-import React, { useState } from 'react';
-import './SignBox.css';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useContext } from "react";
+import "./SignBox.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import AuthContext from "../../context/AuthContext";
+import { useAlert } from "../../context/AlertContext";
 
 const LoginBox = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
+  const { addAlert } = useAlert();
 
-    const handleLogin = async (event) => {
-        event.preventDefault();
+  const handleLogin = async (event) => {
+    event.preventDefault();
 
-        if (!username || !password) {
-            setError('All fields are required');
-            return;
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/v1/users/login`,
+        { username, password }
+      );
+      const { token, role } = response.data;
+
+      sessionStorage.setItem("authToken", token);
+      sessionStorage.setItem("username", username);
+
+      const successMessage =
+        role === "Admin" ? "Login Successful!" : "Login Successful!";
+      addAlert(successMessage, "success", "signbox");
+
+      const infoMessage =
+        role === "Admin"
+          ? "Welcome Admin. Redirecting to the Admin Dashboard..."
+          : "Welcome to the chat! Redirecting to the Chat...";
+
+      addAlert(infoMessage, "info", "signbox");
+
+      setTimeout(() => {
+        login({ username, role });
+        if (role === "Admin") {
+          navigate("/admin");
+        } else {
+          navigate("/chat");
         }
+      }, 1000);
+    } catch (error) {
+      setUsername("");
+      setPassword("");
+      console.error(error.response.data.error);
+      addAlert(error.response.data.error, "error", "signbox");
+    }
+  };
 
-        try {
-            const url = 'http://localhost:5000/api/users/login';
-            const response = await axios.post(url, { username, password }
-            );
-            if (response.status === 200) {
-                const { token } = response.data;
-                localStorage.setItem('authToken', token);
-                navigate('/');
-            }
-        } catch (error) {
-            if (error.response && error.response.data) {
-                setUsername('');
-                setPassword('');
-                setError(error.response.data.msg);
-            } else {
-                setError('Server error');
-            }
-        }
-    };
+  const handleInputChange = (setter) => (event) => {
+    setter(event.target.value);
+  };
 
-    const handleInputChange = (setter) => (event) => {
-        setter(event.target.value);
-        setError('');
-    };
-
-    return (
-        <div className='signerror__container'>
-            <div className='signbox__container'>
-                <div className='signbox__title'>Please Login to Continue</div>
-                <div className='signbox__email signbox__input'>
-                    <label htmlFor="username">Username</label>
-                    <input
-                        type='text'
-                        id='username'
-                        placeholder='Enter your Username'
-                        value={username}
-                        onChange={handleInputChange(setUsername)}
-                        required
-                    />
-                </div>
-                <div className='signbox__password signbox__input'>
-                    <label htmlFor="pass">Password</label>
-                    <input
-                        type='password'
-                        id='pass'
-                        placeholder='Enter your Password'
-                        value={password}
-                        onChange={handleInputChange(setPassword)}
-                        required
-                    />
-                </div>
-                <button className='signbox__button' onClick={handleLogin}>Login</button>
-            </div>
-            <div className='signbox__footer'>No account yet? <Link to='/register'>Register</Link></div>
-            {error && <div className='signbox__errorbox'>{error}</div>}
+  return (
+    <div className="signerror__container">
+      <div className="signbox__container">
+        <div className="signbox__title">Please Login to Continue</div>
+        <div className="signbox__input">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your Username"
+            value={username}
+            onChange={handleInputChange(setUsername)}
+            required
+          />
         </div>
-    );
+        <div className="signbox__input">
+          <label htmlFor="pass">Password</label>
+          <input
+            type="password"
+            id="pass"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={handleInputChange(setPassword)}
+            required
+          />
+        </div>
+        <button className="signbox__button" onClick={handleLogin}>
+          Login
+        </button>
+      </div>
+      <div className="signbox__footer">
+        No account yet? <Link to="/register">Register</Link>
+      </div>
+    </div>
+  );
 };
 
 export default LoginBox;
--- src/components/SignBox/RegisterBox.jsx
+++ src/components/SignBox/RegisterBox.jsx
@@ -1,95 +1,129 @@
-import React, { useState } from 'react';
-import './SignBox.css';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState } from "react";
+import "./SignBox.css";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import AlertBox from "../AlertBox/AlertBox";
 
 const RegisterBox = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [coupon, setCoupon] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [coupon, setCoupon] = useState("");
+  const [messages, setMessages] = useState([]);
+  const navigate = useNavigate();
 
-    const handleRegister = async (event) => {
-        event.preventDefault();
+  const handleRegister = async (event) => {
+    event.preventDefault();
 
-        if (!username || !password || !email || !coupon) {
-            setError('All fields are required');
-            return;
-        }
-        
-        try {
-            const url = 'http://localhost:5000/api/users/register';
-            const response = await axios.post(url, { username, password, email, coupon });
-            if (response.status === 201) {
-                navigate('/login');
-            }
-        } catch (error) {
-            if (error.response && error.response.data) {
-                setError(error.response.data.msg);
-            } else {
-                setError('Server error');
-            }
-        }
-    };
+    if (!username || !password || !email || !coupon) {
+      addMessage("All fields are required", "error");
+      return;
+    }
 
-    return (
-        <div className='signerror__container'>
-            <div className='signbox__container'>
-                <div className='signbox__title'>Sign up for an account!</div>
-                <div className='signbox__email signbox__input'>
-                    <label htmlFor="username">*Username</label>
-                    <input
-                        type='text'
-                        id='username'
-                        placeholder='Enter your Username'
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        required={true}
-                    />
-                </div>
-                <div className='signbox__password signbox__input'>
-                    <label htmlFor="pass">*Password</label>
-                    <input
-                        type='password'
-                        id='pass'
-                        placeholder='Enter your Password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required={true}
-                    />
-                </div>
-                <div className='signbox__coupon signbox__input'>
-                    <label htmlFor="email">*Email</label>
-                    <input
-                        type='email'
-                        id='email'
-                        placeholder='Enter your Email'
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required={true}
-                    />
-                </div>
-                <div className='signbox__coupon signbox__input'>
-                    <label htmlFor="coupon">
-                        *Coupon <span>[Enter your coupon code to avail credits]</span>
-                    </label>
-                    <input
-                        type='text'
-                        id='coupon'
-                        placeholder='Enter your Coupon code'
-                        value={coupon}
-                        onChange={(e) => setCoupon(e.target.value)}
-                        required={true}
-                    />
-                </div>
-                <button className='signbox__button' onClick={handleRegister}>Create Account</button>
-            </div>
-            <div className='signbox__footer'>Already have an account? <Link to='/login'>Log in</Link></div>
-            {error && <div className='signbox__errorbox'>{error}</div>}
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_URL}/api/v1/users/register`,
+        { username, password, email, couponCode: coupon }
+      );
+      if (response.status === 201) {
+        addMessage("Registration Successful", "success");
+        addMessage("Redirecting to Login Page", "info");
+        setTimeout(() => {
+          navigate("/login");
+        }, 3000);
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        addMessage(error.response.data.msg, "error");
+      } else {
+        addMessage("Server error", "error");
+      }
+    }
+  };
+
+  const addMessage = (message, variant) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { id: Date.now(), message, variant },
+    ]);
+  };
+
+  const removeMessage = (id) => {
+    setMessages((prevMessages) => prevMessages.filter((msg) => msg.id !== id));
+  };
+
+  return (
+    <div className="signerror__container">
+      <div className="signbox__container">
+        <div className="signbox__title">Sign up for an account!</div>
+        <div className="signbox__input">
+          <label htmlFor="username">*Username</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required={true}
+          />
         </div>
-    )
-}
+        <div className="signbox__input">
+          <label htmlFor="pass">*Password</label>
+          <input
+            type="password"
+            id="pass"
+            placeholder="Enter your Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required={true}
+          />
+        </div>
+        <div className="signbox__input">
+          <label htmlFor="email">*Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required={true}
+          />
+        </div>
+        <div className="signbox__input">
+          <label htmlFor="coupon">
+            *Coupon <span>[Enter your coupon code to avail credits]</span>
+          </label>
+          <input
+            type="text"
+            id="coupon"
+            placeholder="Enter your Coupon code"
+            value={coupon}
+            onChange={(e) => setCoupon(e.target.value)}
+            required={true}
+          />
+        </div>
+        <button className="signbox__button" onClick={handleRegister}>
+          Create Account
+        </button>
+      </div>
+      <div className="signbox__footer">
+        Already have an account? <Link to="/login">Log in</Link>
+      </div>
+      <div className="signbox__wrapper">
+        {messages.map((msg) => (
+          <AlertBox
+            key={msg.id}
+            id={msg.id}
+            removeAlert={removeMessage}
+            variant={msg.variant}
+            component="signbox"
+          >
+            {msg.message}
+          </AlertBox>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default RegisterBox;
