57f1ba01987b4a60a45d43ac9ab1769d36bf9df6
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-08 10:14:30 +0530

    Updated useMessages.js

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -1,19 +1,14 @@
 import { useState } from "react";
 import axios from "axios";
 import { useAlert } from "../context/AlertContext";
-import useCredits, { triggerFetchCredits } from "./useCredits";
+import { triggerFetchCredits } from "./useCredits";
 
 const useMessages = () => {
   const [messages, setMessages] = useState([]);
   const token = sessionStorage.getItem("authToken");
   const { addAlert } = useAlert();
-  const credits = useCredits();
 
   const addMessage = async (userMessage) => {
-    if(credits <= 0){
-      addAlert("You have no credits left. Please purchase more to continue using the service.", "error", "signbox");
-      return;
-    }
     const tempMessageId = Date.now();
     setMessages((prevMessages) => [
       ...prevMessages,
@@ -45,12 +40,11 @@ const useMessages = () => {
       );
       triggerFetchCredits();
     } catch (error) {
-      console.error(error);
       setMessages((prevMessages) =>
         prevMessages.filter((msg) => msg.id !== tempMessageId)
       );
       addAlert(
-        error.response?.data?.error || "An error occurred",
+        error.response?.data?.error || error.message,
         "error",
         "signbox"
       );
