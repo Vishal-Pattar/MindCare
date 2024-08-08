297e1a4efc2dd88b35ff717c91ebf5e58475bc23
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-08-08 10:14:29 +0530

    Updated HomeArea.jsx

--- src/components/HomeArea/HomeArea.jsx
+++ src/components/HomeArea/HomeArea.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import OutputBox from '../OutputBox/OutputBox';
-import TypeBox from '../TypeBox/TypeBox';
-import useMessages from '../../hooks/useMessages';
-import { triggerFetchCredits } from '../../hooks/useCredits';
-import withAuthorization from '../../utils/withAuthorization';
-import { Permissions } from '../../utils/roles';
+import React, { useEffect } from "react";
+import OutputBox from "../OutputBox/OutputBox";
+import TypeBox from "../TypeBox/TypeBox";
+import useMessages from "../../hooks/useMessages";
+import { triggerFetchCredits } from "../../hooks/useCredits";
+import withAuthorization from "../../utils/withAuthorization";
+import { Permissions } from "../../utils/roles";
 
 const HomeArea = () => {
-    const [messages, addMessage] = useMessages();
-    triggerFetchCredits();
-    return (
-        <>
-            <TypeBox addMessage={addMessage} />
-            <OutputBox messages={messages} />
-        </>
-    );
+  const [messages, addMessage] = useMessages();
+  useEffect(() => {
+    if (sessionStorage.getItem("authToken")) {
+      triggerFetchCredits();
+    }
+  }, []);
+  return (
+    <>
+      <TypeBox addMessage={addMessage} />
+      <OutputBox messages={messages} />
+    </>
+  );
 };
 
 export default withAuthorization(Permissions.User_Access)(HomeArea);
