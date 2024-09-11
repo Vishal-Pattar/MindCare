e150f6e0711c97ea7e48b7f7cc3dc081e1263ab1
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-09-11 08:41:33 +0530

    Updated roles.js

--- src/utils/roles.js
+++ src/utils/roles.js
@@ -4,6 +4,7 @@ export const Permissions = {
 };
 
 export const rolePermissions = {
+    Root: [Permissions.Admin_Access, Permissions.User_Access],
     Admin: [Permissions.Admin_Access, Permissions.User_Access],
     Member: [Permissions.User_Access],
     Tester: [Permissions.User_Access],
