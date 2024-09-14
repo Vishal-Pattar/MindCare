94bc6a77dc992da8bb2679cabbc5801df7842fc9
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-09-14 15:53:08 +0530

    Updated HomeArea.jsx

--- src/components/HomeArea/HomeArea.jsx
+++ src/components/HomeArea/HomeArea.jsx
@@ -1,22 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import OutputBox from "../OutputBox/OutputBox";
 import TypeBox from "../TypeBox/TypeBox";
-import useMessages from "../../hooks/useMessages";
+import {
+  addMessage,
+  fetchChatHistory,
+  responseRecieved,
+} from "../../slices/messagesSlice";
 import { triggerFetchCredits } from "../../hooks/useCredits";
 import withAuthorization from "../../utils/withAuthorization";
 import { Permissions } from "../../utils/roles";
 
 const HomeArea = () => {
-  const [messages, addMessage] = useMessages();
+  const dispatch = useDispatch();
+  const messages = useSelector((state) => state.messages.messages);
+  const history = useSelector((state) => state.messages.history);
+  const isWordEffectRunning = useSelector(
+    (state) => state.messages.isWordEffectRunning
+  );
+  const [currentResponse, setCurrentResponse] = useState(null);
+
   useEffect(() => {
     if (sessionStorage.getItem("authToken")) {
       triggerFetchCredits();
+      dispatch(fetchChatHistory());
     }
-  }, []);
+  }, [dispatch]);
+
+  const handleAddMessage = (message) => {
+    dispatch(addMessage(message)).then((action) => {
+      setCurrentResponse(action.payload); // Set current response
+    });
+  };
+
+  const handleStop = () => {
+    dispatch(responseRecieved()); // Stop word effect
+    setCurrentResponse(null); // Clear current response
+  };
+
   return (
     <>
-      <TypeBox addMessage={addMessage} />
-      <OutputBox messages={messages} />
+      <TypeBox
+        addMessage={handleAddMessage}
+        isResponsePending={isWordEffectRunning}
+        handleStop={handleStop}
+      />
+      <OutputBox
+        messages={history.concat(messages)}
+        currentResponse={currentResponse}
+        onStopEffect={handleStop}
+      />
     </>
   );
 };
