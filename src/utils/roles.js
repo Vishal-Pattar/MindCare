78e9e713df399eeba1e2c33e288d764cc81e0258
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-09-14 15:53:10 +0530

    Updated roles.js

--- src/utils/roles.js
+++ src/utils/roles.js
@@ -1,11 +1,14 @@
 export const Permissions = {
+    Root_Access: 'Root_Access',
     Admin_Access: 'Admin_Access',
     User_Access: 'User_Access',
+    Free_Access: 'Free_Access',
 };
 
 export const rolePermissions = {
-    Root: [Permissions.Admin_Access, Permissions.User_Access],
-    Admin: [Permissions.Admin_Access, Permissions.User_Access],
-    Member: [Permissions.User_Access],
-    Tester: [Permissions.User_Access],
+    Root: [Permissions.Root_Access, Permissions.Admin_Access, Permissions.User_Access, Permissions.Free_Access],
+    Admin: [Permissions.Admin_Access, Permissions.User_Access, Permissions.Free_Access],
+    Member: [Permissions.User_Access, Permissions.Free_Access],
+    Tester: [Permissions.User_Access, Permissions.Free_Access],
+    Guest: [Permissions.Free_Access],
 };
