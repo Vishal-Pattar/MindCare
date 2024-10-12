1189c67f1148857ad1a625e13b8de8063f1065f8
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-10-12 17:33:00 +0530

    Updated Feedback.jsx

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,49 +1,69 @@
 import React, { useState } from "react";
 import "./Feedback.css";
 import { useAlert } from "../../context/AlertContext";
-import {
-  BsEmojiAngryFill,
-  BsEmojiFrownFill,
-  BsEmojiNeutralFill,
-  BsEmojiSmileFill,
-  BsEmojiLaughingFill,
-} from "react-icons/bs";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import ratingOptions from "./ratingOptions";
 
 const Feedback = () => {
   const [feedbackText, setFeedbackText] = useState("");
+  const [selectedRating, setSelectedRating] = useState(null);
   const { addAlert } = useAlert();
+  const navigate = useNavigate();
 
+  // Handle feedback text change
   const handleFeedbackChange = (e) => {
     setFeedbackText(e.target.value);
   };
 
-  const handleSubmit = () => {
-    if (feedbackText.trim()) {
-      addAlert("Feedback submitted successfully!", "success", "bottom_right");
-    } else {
-      addAlert("Please provide feedback before submitting.", "error", "bottom_right");
-    }
+  // Handle rating selection change
+  const handleRatingChange = (id) => {
+    setSelectedRating(id);
   };
 
-  const ratingOptions = [
-    {
-      id: 1,
-      label: "Terrible",
-      icon: <BsEmojiAngryFill className="feedback__icon" />,
-    },
-    { id: 2, label: "Bad", icon: <BsEmojiFrownFill className="feedback__icon" /> },
-    {
-      id: 3,
-      label: "Okay",
-      icon: <BsEmojiNeutralFill className="feedback__icon" />,
-    },
-    { id: 4, label: "Good", icon: <BsEmojiSmileFill className="feedback__icon" /> },
-    {
-      id: 5,
-      label: "Amazing",
-      icon: <BsEmojiLaughingFill className="feedback__icon" />,
-    },
-  ];
+  // Handle form submission
+  const handleSubmit = async () => {
+    if (!selectedRating) {
+      addAlert(
+        "Please choose a rating before submitting.",
+        "error",
+        "bottom_right"
+      );
+      return;
+    }
+
+    // Retrieve username from session storage
+    const username = sessionStorage.getItem("username");
+    const session = sessionStorage.getItem("session");
+
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const response = await axios.post(`${apiUrl}/api/v1/feed/feedback`, {
+        username,
+        session,
+        rating: selectedRating,
+        feedbackText,
+      });
+
+      if (response.data.status === "success") {
+        addAlert("Feedback submitted successfully!", "info", "bottom_right");
+        setFeedbackText("");
+        setSelectedRating(null);
+
+        sessionStorage.removeItem("username");
+        sessionStorage.removeItem("session");
+
+        navigate("/login");
+      }
+    } catch (error) {
+      console.error(error);
+      addAlert(
+        error.response ? error.response.data.message : error.message,
+        "error",
+        "bottom_right"
+      );
+    }
+  };
 
   return (
     <div className="feedback__container">
@@ -54,8 +74,17 @@ const Feedback = () => {
       <div className="feedback__ratingGroup">
         {ratingOptions.map((option) => (
           <div className="feedback__rating" key={option.id}>
-            <input type="radio" name="rating" id={`rating-${option.id}`} />
-            <label htmlFor={`rating-${option.id}`} className={`feedback__rating${option.id}`}>
+            <input
+              type="radio"
+              name="rating"
+              id={`rating-${option.id}`}
+              onChange={() => handleRatingChange(option.id)}
+              checked={selectedRating === option.id}
+            />
+            <label
+              htmlFor={`rating-${option.id}`}
+              className={`feedback__rating${option.id}`}
+            >
               {option.icon}
               <span>{option.label}</span>
             </label>
