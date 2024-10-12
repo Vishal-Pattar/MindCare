b60399a91efb947f5fa62a3c6acc2339501b73a7
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-10-13 00:49:58 +0530

    Updated decodeFromBase64Url.js

--- src/utils/decodeFromBase64Url.js
+++ src/utils/decodeFromBase64Url.js
@@ -1,7 +1,12 @@
 const decodeFromBase64Url = (base64Url) => {
-  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  const paddedBase64 = base64.padEnd(base64.length + (base64.length % 4), "=");
-  const decodedString = atob(paddedBase64);
+  let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const padding = base64.length % 4;
+  if (padding === 2) {
+    base64 += "==";
+  } else if (padding === 3) {
+    base64 += "=";
+  }
+  const decodedString = atob(base64);
   return JSON.parse(decodedString);
 };
 
