1b3f83f15e01442d42dc0d927f7e0cf3dd997e5b
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-10-12 17:32:59 +0530

    Updated Checkbox.jsx

--- src/components/Feedback/Checkbox.jsx
+++ src/components/Feedback/Checkbox.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Checkbox = ({name}) => {
+const Checkbox = ({ name, checked, onChange }) => {
   return (
     <label className="feedback__checkbox--container">
-      <input type="checkbox" name={name} />
+      <input type="checkbox" name={name} checked={checked} onChange={onChange} />
       <svg viewBox="0 0 64 64" height="1rem" width="1rem">
         <path
           d="M 0 16 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 16 L 32 48 L 64 16 V 8 A 8 8 90 0 0 56 0 H 8 A 8 8 90 0 0 0 8 V 56 A 8 8 90 0 0 8 64 H 56 A 8 8 90 0 0 64 56 V 16"
