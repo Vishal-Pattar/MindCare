1a7ef2c887516d6c650abdaadd23c7a989587258
Vishal-Pattar <vmpnwc15002@gmail.com>
2025-01-11 19:29:51 +0530

    Updated formatDateTime.js

--- src/utils/formatDateTime.js
+++ src/utils/formatDateTime.js
@@ -1,8 +1,13 @@
-import moment from 'moment';
+import moment from "moment";
 
 const formatDateTime = (dateString) => {
-    if (!dateString) return '';
-    return moment(dateString).format('YYYY-MM-DD HH:mm:ss');
+  if (!dateString) return "";
+  return moment(dateString).format("YYYY-MM-DD HH:mm:ss");
+};
+
+export const formatDateTimeinTransaction = (dateString) => {
+  if (!dateString) return "";
+  return moment(dateString).format("Do-MMM-YYYY");
 };
 
 export default formatDateTime;
