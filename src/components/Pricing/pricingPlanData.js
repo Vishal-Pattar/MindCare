297166c85538897c7d3b2fe509ea75b3c7ae8281
Vishal-Pattar <vmpnwc15002@gmail.com>
2025-01-11 19:28:33 +0530

    Added pricingPlanData.js

---
+++ src/components/Pricing/pricingPlanData.js
@@ -0,0 +1,41 @@
+const pricingPlanData = [
+  {
+    name: "Free",
+    price: 0,
+    features: [
+      "10 credits monthly",
+      "Delay in messages",
+      "Email Support (24-48 hours)",
+      "Free Resources",
+      "Access to Community Forum",
+    ],
+  },
+  {
+    name: "Pro",
+    price: 1000,
+    features: [
+      "500 credits monthly",
+      "All features in Free Plan",
+      "Prompt service",
+      "Priority Email Support (12-24 hours)",
+      "Theme customization",
+      "Early Access to New Features",
+      "Basic Usage Reports",
+    ],
+  },
+  {
+    name: "Enterprise",
+    price: "Pay as you go",
+    features: [
+      "All features in Pro Plan",
+      "Pay as per your usage",
+      "24/7 Dedicated Support",
+      "Advanced Customization Options",
+      "Advanced Usage Reports",
+      "Custom Integrations & API Access",
+      "Scalability for Enterprise",
+    ],
+  },
+];
+
+export default pricingPlanData;
