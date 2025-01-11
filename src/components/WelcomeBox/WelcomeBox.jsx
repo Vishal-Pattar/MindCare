06739e220f84d0f2aa259190c776881ec40526a8
Vishal-Pattar <vmpnwc15002@gmail.com>
2025-01-11 19:29:50 +0530

    Updated WelcomeBox.jsx

--- src/components/WelcomeBox/WelcomeBox.jsx
+++ src/components/WelcomeBox/WelcomeBox.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import './WelcomeBox.css';
-import withAuthorization from '../../utils/withAuthorization';
-import { Permissions } from '../../utils/roles';
+import React from "react";
+import "./WelcomeBox.css";
+import withAuthorization from "../../utils/withAuthorization";
+import { Permissions } from "../../utils/roles";
 
-const WelcomeBox = () => {
+const WelcomeBox = ({ addMessage }) => {
+  const prompts = [
+    "I feel lonely. Can you help?",
+    "How can I stop overthinking?",
+    "Why am I feeling so sad all the time?",
+    "What should I do if I feel like crying all the time?",
+  ];
+
   return (
-    <div className='welcomebox__container'>
-        <div className='welcomebox__header'>Welcome to Your Mental Health Companion!</div>
-        <div className='welcomebox__content'>
-            <p>We understand that life can sometimes be overwhelming, and it's important to have someone to talk to. Our AI chatbot is here to provide you with support, guidance, and a listening ear.</p>
-            <p>Whether you're feeling anxious, stressed, or simply need someone to share your thoughts with, our chatbot is designed to help you navigate through these feelings. Remember, you are not alone, and taking the first step towards better mental health is commendable.</p>
-            <p>Feel free to start a conversation, ask questions, or share what’s on your mind. Your well-being is our priority, and we're here to support you every step of the way.</p>
+    <div className="welcomebox__container">
+      <div className="welcomebox__container--one">
+        <div className="welcomebox__header">
+          Welcome to Your Mental Health Companion!
         </div>
+        <div className="welcomebox__content">
+          <p>
+            Life can sometimes feel overwhelming, and it's important to have
+            someone to talk to. Our AI chatbot is here to provide support,
+            guidance, and a listening ear when you need it most.
+          </p>
+          <p>
+            Whether you're feeling anxious, stressed, or simply need to share
+            your thoughts, we're here to help you navigate these emotions.
+            Remember, you are not alone, and taking the first step towards
+            better mental health is a commendable act.
+          </p>
+        </div>
+      </div>
+      <div className="welcomebox__container--two">
+        {prompts.map((prompt, index) => (
+          <div
+            key={index}
+            className="welcomebox__prompts"
+            onClick={() => addMessage(prompt)}
+          >
+            {prompt}
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default withAuthorization(Permissions.User_Access)(WelcomeBox);
