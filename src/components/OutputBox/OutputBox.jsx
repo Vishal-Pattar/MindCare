a540a87dff473e565b755089c7af728a751fb4e2
Vishal-Pattar <vmpnwc15002@gmail.com>
2025-01-11 19:29:47 +0530

    Updated OutputBox.jsx

--- src/components/OutputBox/OutputBox.jsx
+++ src/components/OutputBox/OutputBox.jsx
@@ -1,62 +1,16 @@
-import React, { useState, useEffect, useRef, useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useRef } from "react";
 import WelcomeBox from "../WelcomeBox/WelcomeBox";
 import "./OutputBox.css";
 import loading from "../../assets/loader.gif";
 import Markdown from "markdown-to-jsx";
 import { BsPersonCircle } from "react-icons/bs";
 import { RiRobot2Fill } from "react-icons/ri";
-import { responseRecieved, startWordEffect } from "../../slices/messagesSlice";
 import { triggerFetchCredits } from "../../hooks/useCredits";
 import withAuthorization from "../../utils/withAuthorization";
 import { Permissions } from "../../utils/roles";
 
-const OutputBox = ({ messages, currentResponse, onStopEffect }) => {
-  const dispatch = useDispatch();
-  const isWordEffectRunning = useSelector(
-    (state) => state.messages.isWordEffectRunning
-  );
-  const [currentOutput, setCurrentOutput] = useState("");
+const OutputBox = ({ messages, currentResponse, addMessage }) => {
   const containerRef = useRef(null);
-  const timeoutRef = useRef(null);
-
-  const runWordEffect = useCallback(() => {
-    if (currentResponse && currentResponse.output) {
-      const words = currentResponse.output.split(" ");
-      let index = 0;
-      setCurrentOutput("");
-
-      dispatch(startWordEffect());
-      const wordEffect = () => {
-        if (index < words.length) {
-          setCurrentOutput((prev) => prev + words[index] + " ");
-          index++;
-          timeoutRef.current = setTimeout(wordEffect, 150);
-        } else {
-          dispatch(responseRecieved());
-          setCurrentOutput(currentResponse.output);
-          onStopEffect();
-        }
-      };
-
-      wordEffect();
-    }
-  }, [currentResponse, dispatch, onStopEffect]);
-
-  const handleStop = () => {
-    clearTimeout(timeoutRef.current);
-    setCurrentOutput(currentResponse.output);
-    dispatch(responseRecieved());
-    onStopEffect();
-  };
-
-  useEffect(() => {
-    if (currentResponse && isWordEffectRunning) {
-      runWordEffect();
-    }
-    return () => clearTimeout(timeoutRef.current);
-  }, [currentResponse, isWordEffectRunning, runWordEffect]);
-
   useEffect(() => {
     if (containerRef.current) {
       containerRef.current.scrollTop = containerRef.current.scrollHeight;
@@ -68,21 +22,19 @@ const OutputBox = ({ messages, currentResponse, onStopEffect }) => {
   }, [currentResponse]);
 
   return messages.length === 0 ? (
-    <WelcomeBox />
+    <WelcomeBox addMessage={addMessage} />
   ) : (
     <div className="outputbox__container" ref={containerRef}>
       {messages.map((msg, index) => (
         <div key={index} className="outputbox__content">
-          <span>
+          <span className="outputbox__pr">
             <BsPersonCircle className="outputbox__image" />
             <div className="outputbox__textspace">{msg.prompt}</div>
           </span>
-          <span>
+          <span className="outputbox__pr">
             <RiRobot2Fill className="outputbox__image" />
             <div className="outputbox__textspace outputbox__textspace_output">
-              {index === messages.length - 1 && isWordEffectRunning ? (
-                <Markdown>{currentOutput}</Markdown>
-              ) : msg.response === "Loading..." ? (
+              {msg.response === "Loading..." ? (
                 <img src={loading} alt="loading" className="loader" />
               ) : (
                 <Markdown>{msg.response}</Markdown>
